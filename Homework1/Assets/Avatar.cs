afb8c455b24a3193810039abc939f1405f929c46
Sacha <sachabest@outlook.com>
2016-01-21 21:00:14 -0500

    Add variable gravity

--- Homework1/Assets/Avatar.cs
+++ Homework1/Assets/Avatar.cs
@@ -3,11 +3,6 @@ using System.Collections;
 
 public class Avatar : MonoBehaviour {
 
-	public KeyCode Forward;
-	public KeyCode Backward;
-	public KeyCode Left;
-	public KeyCode Right;
-
 	public KeyCode SpinLeft;
 	public KeyCode SpinRight;
 	public KeyCode FlipUp;
@@ -15,28 +10,32 @@ public class Avatar : MonoBehaviour {
 
 	public KeyCode Pop;
 
-	public Rigidbody PersonRigidBody;
+	public WheelCollider[] WHeelColliders;
 
-	private Vector3 _speedVector;
-	private Vector3 _leanVector;
+	private float _maxTorque = 5000f;
 
 	// Use this for initialization
 	void Start () {
-		_speedVector = new Vector3(400.0f, 0.0f, 0.0f);
-		_leanVector = new Vector3(0.0f, 0.0f, 50.0f);
 	}
 	
-	// Update is called once per frame
-	void Update() {
-		if (Input.GetKey(Forward)) {
-			this.PersonRigidBody.AddForce(-1 * _speedVector);
-		} if (Input.GetKey(Backward)) {
-			this.PersonRigidBody.AddForce(_speedVector);
-		} if (Input.GetKey(Left)) {
-			this.PersonRigidBody.AddForce(-1 * _leanVector);
-		} if (Input.GetKey(Right)) {
-			this.PersonRigidBody.AddForce(_leanVector);
-		}
-	}
+    void FixedUpdate()
+    {
+        float steer = Input.GetAxis("Horizontal");
+        float accelerate = Input.GetAxis("Vertical");
+
+        float finalAngle = steer * 5f;
+        WHeelColliders[0].steerAngle = finalAngle;
+        WHeelColliders[1].steerAngle = finalAngle;
+
+        if (steer != 0) {
+            return;
+            // cant speed up while turning
+        }
+
+        for(int i = 0; i < 4; i++)
+        {
+            WHeelColliders[i].motorTorque = accelerate * _maxTorque;
+        }
+    }
 
 }
---
+++ Homework1/Assets/GaemManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GaemManager : MonoBehaviour {
+
+	public KeyCode Toggle = KeyCode.LeftShift;
+	public KeyCode AltToggle = KeyCode.RightShift;
+	public KeyCode[] Gravity = { KeyCode.W, KeyCode.S, KeyCode.A, KeyCode.D };
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(Toggle) || Input.GetKey(AltToggle)) {
+			if (Input.GetKeyDown(Gravity[0])) {
+				Physics.gravity = new Vector3(0, 9.8f, 0);
+			}
+			if (Input.GetKeyDown(Gravity[1])) {
+				Physics.gravity = new Vector3(0, -9.8f, 0);
+			}
+			if (Input.GetKeyDown(Gravity[2])) {
+				Physics.gravity = new Vector3(0, 0, -9.8f);
+			}
+			if (Input.GetKeyDown(Gravity[3])) {
+				Physics.gravity = new Vector3(0, 0, 9.8f);
+			}
+		}
+	}
+}
